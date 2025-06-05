205c52ac9b20d98fee1041b2bfb1c0f21de9d3a0
NathanielAB <73097613+NathanielAB@users.noreply.github.com>
2025-06-05 21:56:10 +0200

    StockPrices Retrieval Microservice Initial Implementation

---
+++ StockPriceMonitoring.StockPrices/Stock.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace StockPriceMonitoring.StockPrices {
+    internal sealed class Stock {
+        [JsonProperty("symbol")]
+        public string Symbol { get; set; } = null!;
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+    }
+}
